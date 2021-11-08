7f6b3984c1d1273698423ae22801207fbf8afc87
yilianz <yilianz@gmail.com>
2021-11-08 12:34:23 -0500

    done the implementation of list

--- js/add-element.js
+++ js/add-element.js
@@ -10,16 +10,28 @@ $('.hot').each(function () {
 
 // traverse the elements
 $('#one').next().next().text('milk');
+//$('#one').remove();
 // add a new element by clicking the plus sign
 $('#todo').append('<li>candy</li>');
-
+$('#add').click(addElement);
 // before and after are for siblings
 // append and prepend are for parent
 
 function addElement() {
   // add a new element
   // add a input text box
+  $('#todo').append('<li><input type="text"></li>');
   // whenever the user are done add the element
+  $('input').blur(function(){
+    $(this).parent().addClass('cool');
+    var items = $(this).val();
+    //replace the input box by the text
+    $(this).parent().text(items);
+
+    // click the new list will change the style
+
+  });
+
 }
 
 // bind click with the event handler
@@ -38,4 +50,6 @@ document.getElementById('remove').addEventListener('click', removeElement);
 
 function removeElement() {
   // remove the marked elements  -- element with style complete
+ $('li.complete').remove();
+
 }
